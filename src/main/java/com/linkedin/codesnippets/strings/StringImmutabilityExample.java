519a46ba30117a0a03f451f4489f205387bfabdd
pedrolopes9-7 <pedroinf97@gmail.com>
2023-02-02 23:22:49 -0300

    adding resources

--- src/main/java/com/linkedin/codesnippets/strings/StringImmutabilityExample.java
+++ src/main/java/com/linkedin/codesnippets/strings/StringImmutabilityExample.java
@@ -1,16 +1,27 @@
 package com.linkedin.codesnippets.strings;
 
+/*@author: pedrolopesdev at 2023-02-02
+ Follow for more tips:
+ https://www.linkedin.com/in/pedro-lopes-dev//
+ https://twitter.com/pedro_lopes3
+ https://pedrolopesdev.com */
 public class StringImmutabilityExample {
 
-    //Explanation: We should use core concepts of Java String to answer this question:
-    //  - Strings are immutable, so trying to modify `hello` using `concat` doesn't work like on line `9`.
-    //     Thus, line `16` prints false.
-    //  - The `equals` method compares the content of two String objects.
-    //     Thus, lines `17` and `18` prints true.
-    //  - The `==` operator compares the objects' addresses, not their content. Using `new String()` and `new StringBuilder()` instantiates new addresses in memory.
-    //     Therefore, lines `19` and `20` print false.
-    //  - The particular case for the `==` operator is when the two Strings are created using double quotes, aka literals.
-    //     In that case, the String Pool assigns the same memory address to both variables. Thus, line `21` prints true.
+    /* Explanation: We should use core concepts of Java String to answer this question:
+      - Strings are immutable, so trying to modify `hello` using `concat` doesn't work like on line `9`.
+         Thus, line `16` prints false.
+      - The `equals` method compares the content of two String objects.
+         Thus, lines `17` and `18` prints true.
+      - The `==` operator compares the objects' addresses, not their content. Using `new String()` and `new StringBuilder()` instantiates new addresses in memory.
+         Therefore, lines `19` and `20` print false.
+      - The particular case for the `==` operator is when the two Strings are created using double quotes, aka literals.
+         In that case, the String Pool assigns the same memory address to both variables. Thus, line `21` prints true. */
+
+    /* Check out useful resources:
+     https://pedrolopesdev.com/post/java-string-pool
+     https://pedrolopesdev.com/post/string-concatenation-in-java/ */
+
+
     public static void main(String[] args) {
         String hello = "Hello";
         String world = " World";
