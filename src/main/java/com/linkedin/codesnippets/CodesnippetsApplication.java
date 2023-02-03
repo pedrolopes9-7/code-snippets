9e8d6c293392b189badef240f0e7ba5635ecfdd3
pedrolopes9-7 <pedroinf97@gmail.com>
2023-02-02 22:29:52 -0300

    init

---
+++ src/main/java/com/linkedin/codesnippets/CodesnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.codesnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesnippetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedin/codesnippets/CodesnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.codesnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
