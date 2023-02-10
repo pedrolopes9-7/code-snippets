14e0c785f8321b8bfc0d9123e9ed191ef39db91c
pedrolopes9-7 <pedroinf97@gmail.com>
2023-02-10 10:47:16 -0300

    short circuits

--- src/main/java/com/linkedin/codesnippets/operators/ShortCircuitOperatorExample.java
+++ src/main/java/com/linkedin/codesnippets/operators/ShortCircuitOperatorExample.java
@@ -2,7 +2,7 @@ package com.linkedin.codesnippets.operators;
 
 import java.util.List;
 
-/*@author: pedrolopesdev at 2023-02-07
+/*@author: pedrolopesdev at 2023-02-10
  https://www.linkedin.com/in/pedro-lopes-dev//
  https://twitter.com/pedro_lopes3
  https://pedrolopesdev.com */
@@ -10,10 +10,10 @@ public class ShortCircuitOperatorExample {
 
     /*
         What's the output of the following program?
-        A) NullPointerException at line //1
-        B) Printing output...
-        C) IndexOutOfBoundsException at line //2
-        D) Nothing
+        A) NullPointerException at 1
+        B) First number: 2
+        C) IndexOutOfBoundsException at 2
+        D) NullPointerException at 3
 
         Useful Resources:
         https://pedrolopesdev.com/post/short-circuit-operators-in-java/
@@ -22,12 +22,15 @@ public class ShortCircuitOperatorExample {
     public static void main(String[] args) {
         List<Integer> myList = null;
 
-        if (isEven(10) || myList.get(0) == 2) { //1
+        if ( ( isEven(10) || myList.get(0) == 2 ) || myList.get(1) == 4 ) { //1
             myList = List.of(2, 3);
         }
-        if (myList.contains(4) && isEven(myList.get(6))) { //2
-            System.out.println("Printing output...");
+
+        if ( ( isEven(myList.get(1)) && myList.contains(3) ) && isEven(myList.get(3)) ) { //2
+            myList = null;
         }
+
+        System.out.println("First number: " + myList.get(0)); //3
     }
 
     private static boolean isEven(int number) {
@@ -36,12 +39,20 @@ public class ShortCircuitOperatorExample {
 
     /*
         Explanation:
-        The answer is nothing.
+        The program doesn't throw any exception and prints out `Printing output...`.
 
-        The call to `isEven(10)` evaluates to true, so the OR (||) operator short-circuits the expression and skip evaluating `myList.get(0) == 2`.
-        Therefore, the whole if is true and no exception is thrown at //1.
+        The answer is to understand that logical OR (||) short circuits when `true`, whereas logical AND (&&) short circuits when `false`
 
-        The call to `myList.contains(4)` is false, so the AND (&&) operator short-circuits the expression and skip the call to `isEven(myList.get(6))`.
-        Therefore, the whole if is false and no exception is thrown at //2.
+        First `if` statement:
+            - The call to `isEven(10)` evaluates to true, making the whole first logical OR (||)
+                expression true, skipping the call to `myList.get(0) == 2`.
+            - There's another OR, so it'll also skip the call to `myList.get(1) == 4`. Therefore, the first statement
+                is TRUE, and `myList` is initialized with `List.of(2,3)`.
+
+        Second `if` statement:
+            The opposite of logical OR (||) occurs with logical AND (&&). We skip the next operands of the conditional
+                if the first is `false`, making the entire expression false.
+            - `isEven(myList.get(1))` is false, thus `( isEven(myList.get(1)) && myList.contains(3) )` is also false.
+            - Same thing for the second logical AND. The previous result is false, so it skips the call to `isEven(myList.get(3)).
      */
 }
